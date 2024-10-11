987fee7f8e1e02315aa382622d394fd089aea87e
Djalma <djalmahy@hotmail.com>
2024-10-11 20:58:37 -0300

    FOOTER ongoing

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from '../../components/navbar';
+import Footer from '../../components/footer';
+import './style.css';
 
 export default function Layout() {
     return (
         <div className='layout'>
             <NavBar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
